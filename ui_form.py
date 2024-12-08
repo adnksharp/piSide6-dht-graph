507ab929fef619b73e53fffb1caff814a0d15480
adnksharp <darkmark404@protonmail.com>
2024-12-07 22:42:25 -0700

    feat: display data.

--- ui_form.py
+++ ui_form.py
@@ -22,7 +22,8 @@ class Ui_Widget(object):
     def setupUi(self, Widget):
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
-        Widget.resize(538, 337)
+        Widget.resize(538, 346)
+        Widget.setStyleSheet(u"")
         self.verticalLayout_6 = QVBoxLayout(Widget)
         self.verticalLayout_6.setObjectName(u"verticalLayout_6")
         self.horizontalLayout = QHBoxLayout()
@@ -39,24 +40,24 @@ class Ui_Widget(object):
 
         self.verticalLayout_2.addWidget(self.label)
 
-        self.line = QFrame(Widget)
-        self.line.setObjectName(u"line")
-        self.line.setFrameShape(QFrame.Shape.HLine)
-        self.line.setFrameShadow(QFrame.Shadow.Sunken)
+        self.dhtH = QLabel(Widget)
+        self.dhtH.setObjectName(u"dhtH")
+        sizePolicy.setHeightForWidth(self.dhtH.sizePolicy().hasHeightForWidth())
+        self.dhtH.setSizePolicy(sizePolicy)
+        self.dhtH.setStyleSheet(u"font: 28pt \"JetBrainsMono Nerd Font Mono\";")
+        self.dhtH.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
 
-        self.verticalLayout_2.addWidget(self.line)
-
-        self.label_2 = QLabel(Widget)
-        self.label_2.setObjectName(u"label_2")
-        sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
-        self.label_2.setSizePolicy(sizePolicy)
-        self.label_2.setStyleSheet(u"font: 28pt \"JetBrainsMono Nerd Font Mono\";")
-        self.label_2.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
-
-        self.verticalLayout_2.addWidget(self.label_2)
+        self.verticalLayout_2.addWidget(self.dhtH)
 
 
         self.horizontalLayout.addLayout(self.verticalLayout_2)
+
+        self.line_2 = QFrame(Widget)
+        self.line_2.setObjectName(u"line_2")
+        self.line_2.setFrameShape(QFrame.Shape.VLine)
+        self.line_2.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.horizontalLayout.addWidget(self.line_2)
 
         self.verticalLayout_3 = QVBoxLayout()
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
@@ -68,27 +69,27 @@ class Ui_Widget(object):
 
         self.verticalLayout_3.addWidget(self.label_3)
 
-        self.line_2 = QFrame(Widget)
-        self.line_2.setObjectName(u"line_2")
-        self.line_2.setFrameShape(QFrame.Shape.HLine)
-        self.line_2.setFrameShadow(QFrame.Shadow.Sunken)
-
-        self.verticalLayout_3.addWidget(self.line_2)
-
-        self.label_4 = QLabel(Widget)
-        self.label_4.setObjectName(u"label_4")
+        self.dhtC = QLabel(Widget)
+        self.dhtC.setObjectName(u"dhtC")
         sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Maximum)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.label_4.sizePolicy().hasHeightForWidth())
-        self.label_4.setSizePolicy(sizePolicy1)
-        self.label_4.setStyleSheet(u"font: 48pt \"JetBrainsMono Nerd Font Mono\";")
-        self.label_4.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        sizePolicy1.setHeightForWidth(self.dhtC.sizePolicy().hasHeightForWidth())
+        self.dhtC.setSizePolicy(sizePolicy1)
+        self.dhtC.setStyleSheet(u"font: 48pt \"JetBrainsMono Nerd Font Mono\";")
+        self.dhtC.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.verticalLayout_3.addWidget(self.label_4)
+        self.verticalLayout_3.addWidget(self.dhtC)
 
 
         self.horizontalLayout.addLayout(self.verticalLayout_3)
+
+        self.line_3 = QFrame(Widget)
+        self.line_3.setObjectName(u"line_3")
+        self.line_3.setFrameShape(QFrame.Shape.VLine)
+        self.line_3.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.horizontalLayout.addWidget(self.line_3)
 
         self.verticalLayout_4 = QVBoxLayout()
         self.verticalLayout_4.setObjectName(u"verticalLayout_4")
@@ -100,26 +101,26 @@ class Ui_Widget(object):
 
         self.verticalLayout_4.addWidget(self.label_5)
 
-        self.line_3 = QFrame(Widget)
-        self.line_3.setObjectName(u"line_3")
-        self.line_3.setFrameShape(QFrame.Shape.HLine)
-        self.line_3.setFrameShadow(QFrame.Shadow.Sunken)
+        self.dhtF = QLabel(Widget)
+        self.dhtF.setObjectName(u"dhtF")
+        sizePolicy.setHeightForWidth(self.dhtF.sizePolicy().hasHeightForWidth())
+        self.dhtF.setSizePolicy(sizePolicy)
+        self.dhtF.setStyleSheet(u"font: 28pt \"JetBrainsMono Nerd Font Mono\";")
 
-        self.verticalLayout_4.addWidget(self.line_3)
-
-        self.label_6 = QLabel(Widget)
-        self.label_6.setObjectName(u"label_6")
-        sizePolicy.setHeightForWidth(self.label_6.sizePolicy().hasHeightForWidth())
-        self.label_6.setSizePolicy(sizePolicy)
-        self.label_6.setStyleSheet(u"font: 28pt \"JetBrainsMono Nerd Font Mono\";")
-
-        self.verticalLayout_4.addWidget(self.label_6)
+        self.verticalLayout_4.addWidget(self.dhtF)
 
 
         self.horizontalLayout.addLayout(self.verticalLayout_4)
 
 
         self.verticalLayout_6.addLayout(self.horizontalLayout)
+
+        self.line = QFrame(Widget)
+        self.line.setObjectName(u"line")
+        self.line.setFrameShape(QFrame.Shape.HLine)
+        self.line.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.verticalLayout_6.addWidget(self.line)
 
         self.frame = QFrame(Widget)
         self.frame.setObjectName(u"frame")
@@ -149,10 +150,10 @@ class Ui_Widget(object):
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"DHT11", None))
         self.label.setText(QCoreApplication.translate("Widget", u"Humedad (%)", None))
-        self.label_2.setText(QCoreApplication.translate("Widget", u"0.00 ", None))
+        self.dhtH.setText(QCoreApplication.translate("Widget", u"0.00 ", None))
         self.label_3.setText(QCoreApplication.translate("Widget", u"Temperatura (\u00b0C)", None))
-        self.label_4.setText(QCoreApplication.translate("Widget", u"0.00", None))
+        self.dhtC.setText(QCoreApplication.translate("Widget", u"0.00", None))
         self.label_5.setText(QCoreApplication.translate("Widget", u"Temperatura (\u00b0F)", None))
-        self.label_6.setText(QCoreApplication.translate("Widget", u"0.00", None))
+        self.dhtF.setText(QCoreApplication.translate("Widget", u"0.00", None))
     # retranslateUi
 
--- widget.py
+++ widget.py
@@ -25,31 +25,41 @@ SPin = 2
 SType = 11
 
 class Canvas(FigureCanvas):
-    def __init__(self, *args):
+    def __init__(self):
         fig = Figure(facecolor=QPalette().color(QPalette.Window).name())
         fig.set_tight_layout(True)
 
         self.axes = fig.add_subplot(111, facecolor=QPalette().color(QPalette.Window).name())
-        self.axes.tick_params(labelcolor=colors['off'], color=colors['on'])
-
+        self.axes.get_xaxis().set_visible(False)
+        self.axes.get_yaxis().set_visible(False)
+        
         for spine in self.axes.spines.values():
-            spine.set_edgecolor(colors['high'])
+            spine.set_edgecolor(QPalette().color(QPalette.Window).name())
 
         super(Canvas, self).__init__(fig)
 
-    def update(self, xp, data, lim):
+    def update(self, xp, data):
         self.axes.clear()
-        self.axes.plot(xp, data, color=QPalette().color(QPalette.Text).name(), linewidth=2)
-        self.axes.set_xlim([lim[0], lim[1]])
+        self.axes.plot(xp, data, color=colors['on'], linewidth=2)
+        self.axes.set_xlim(0, 29.5)
+        if SType == 11:
+            self.axes.set_ylim(0, 59)
+        else:
+            self.axes.set_ylim(-40, 125)
 
 class Board():
-    def __init__(self):
+    def __init__(self, parent):
+        self.parent = parent
+        
         self.data = []
         self.ino = arduino.Pymata4()
         self.ino.set_pin_mode_dht(SPin, sensor_type=SType, callback=self.update)
         
     def update(self, data):
         self.data = [data[4], data[5], (data[5]* 9 / 5) + 32]
+        self.parent.dhtH.setText(str(self.data[0]))
+        self.parent.dhtC.setText(str(self.data[1]))
+        self.parent.dhtF.setText(str(self.data[2]))
 
 class Widget(QWidget):
     def __init__(self, parent=None):
@@ -57,8 +67,36 @@ class Widget(QWidget):
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
         
-        self.board = Board()
+        self.label = [i for i in range(60)]
+        self.temp = [0 for _ in range(60)]
+        # self.hum = [0 for _ in range(60)]
         
+        self.board = Board(self.ui)
+        
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.updates)
+        self.timer.start(1000)
+        
+    def clearHeaders(self, layout):
+        if layout is not None:
+            for i in reversed(range(layout.count())):
+                item = layout.itemAt(i)
+                if item.widget():
+                    widget = item.widget()
+                    widget.deleteLater()
+                elif item.layout():
+                    self.clearHeaders(item.layout())
+        
+    def updates(self):
+        self.temp.pop()
+        # self.hum.pop()
+        self.temp.insert(0, self.board.data[1])
+        # self.hum.insert(0, self.sensor.data[1])
+        
+        graph = Canvas()
+        graph.update(self.label, self.temp)
+        self.clearHeaders(self.ui.graphs)
+        self.ui.graphs.addWidget(graph)
         
     def closeEvent(self, event):
         self.board.ino.shutdown()
